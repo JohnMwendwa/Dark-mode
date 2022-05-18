862774676f267e7af41604011dcd86641dd6f395
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-05-18 07:33:05 +0300

    import Navbar stylesheet

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,13 @@
 import React, { useContext } from 'react';
 import {ThemeContext} from './contexts/ThemeContext';
 import {LanguageContext} from './contexts/LanguageContext';
-
+import './Navbar.css'
 
 export default function Navbar() {
   const {language,changeLanguage} = useContext(LanguageContext);
   const {isDarkMode,toggleTheme} = useContext(ThemeContext)
   return (
-    <div className='Navbar' >
+    <div className={isDarkMode ?'Navbar dark' : 'Navbar light'} >
       <select value={language} onChange={changeLanguage}>
         <option value="english">Kenya</option>
         <option value="french">France</option>
@@ -15,8 +15,8 @@ export default function Navbar() {
       </select>
       
       <label htmlFor="checkbox">
-        {isDarkMode? 'dark' :'light'}
-        <input type="checkbox"  onClick={toggleTheme} />
+        {isDarkMode? 'Dark' :'Light'}
+        <input type="checkbox"  onClick={toggleTheme}  className='Navbar-input' />
       </label>
     </div>
   )

fd6dca82f29d6a8e58e105725429441154278b7e
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-05-18 09:30:33 +0300

    import theme context

--- src/Form.js
+++ src/Form.js
@@ -1,22 +1,25 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { ThemeContext } from './contexts/ThemeContext'
 
 export default function Form() {
+  const {isDarkMode} = useContext(ThemeContext)
   return (
-    <div className='Form'>
-
-      <h1 className="title">Sign In</h1>
+    <div className='Form-wrapper'>
+       <div className={isDarkMode ? "Form dark" : "Form light"}>
+      <h1 className="Form-title">Sign In</h1>
 
       <div className="inputContainer">
-        <input type="text" className="input" placeholder="a" />
-        <label htmlFor="" className="label">Email</label>
+        <input type="text" className="Form-input" placeholder="a" />
+        <label htmlFor="" className={isDarkMode?"label dark":"label light"}>Email</label>
       </div>
 
       <div class="inputContainer">
-        <input type="text" className="input" placeholder="a" />
-        <label htmlFor="" className="label">Password</label>   
+        <input type="text" className="Form-input" placeholder="a" />
+        <label htmlFor="" className={isDarkMode?"label dark":"label light"}>Password</label>   
       </div>
 
     <button className='submitBtn'>Sign In</button>
+    </div>
     </div>
   )
 }

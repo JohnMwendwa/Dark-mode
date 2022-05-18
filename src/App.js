d763ba1dc577610d7a27a9d722295d2fac42d3e2
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-05-18 06:35:37 +0300

    wrapp all components with the theme provider context

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import './App.css';
 import Navbar from './Navbar';
 import PageContent from './PageContent';
 import {LanguageProvider} from './contexts/LanguageContext'
+import { ThemeProvider } from './contexts/ThemeContext';
 
 function App() {
   return (
     <div className="App">
+      <ThemeProvider>
       {/* Wrap all components that are going to consume the context with the LanguageProvider */}
-      <LanguageProvider>
-        <PageContent>
-          <Navbar />
-        </PageContent>
-    </LanguageProvider>
+        <LanguageProvider>
+          <PageContent>
+            <Navbar />
+          </PageContent>
+      </LanguageProvider>
+    </ThemeProvider>
     </div>
   );
 }

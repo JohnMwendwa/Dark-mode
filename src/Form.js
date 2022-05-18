e021a59cd195c2e5f4ce82d7e8e027fbbe1bb959
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-05-18 10:27:53 +0300

    implement language context to dynamically change according to region selected

--- src/Form.js
+++ src/Form.js
@@ -1,24 +1,54 @@
 import React,{useContext} from 'react'
-import { ThemeContext } from './contexts/ThemeContext'
+import { ThemeContext } from './contexts/ThemeContext';
+import { LanguageContext } from './contexts/LanguageContext';
+
+
+const words = {
+  english: {
+    signIn: "Sign In",
+    email: "Email Address",
+    password: "Password",
+    remember: "Remember Me"
+  },
+  french: {
+    signIn: "Se Connecter",
+    email: "Adresse Électronique",
+    password: "Mot de Passe",
+    remember: "Souviens-toi De Moi"
+  },
+  spanish: {
+    signIn: "Registrarse",
+    email: "Correo Electrónico",
+    password: "Contraseña",
+    remember: "Recuérdame"
+  }
+};
 
 export default function Form() {
-  const {isDarkMode} = useContext(ThemeContext)
+  const {isDarkMode} = useContext(ThemeContext);
+  const {language} = useContext(LanguageContext);
+  const {email,password,signIn,remember} = words[language];
   return (
     <div className='Form-wrapper'>
        <div className={isDarkMode ? "Form dark" : "Form light"}>
-      <h1 className="Form-title">Sign In</h1>
+      <h1 className="Form-title">{signIn}</h1>
 
       <div className="inputContainer">
-        <input type="text" className="Form-input" placeholder="a" />
-        <label htmlFor="" className={isDarkMode?"label dark":"label light"}>Email</label>
+        <input type="text" className="Form-input" placeholder="email" />
+        <label htmlFor="" className={isDarkMode?"label dark":"label light"}>{email}</label>
       </div>
 
       <div class="inputContainer">
-        <input type="text" className="Form-input" placeholder="a" />
-        <label htmlFor="" className={isDarkMode?"label dark":"label light"}>Password</label>   
+        <input type="text" className="Form-input" placeholder="password" />
+        <label htmlFor="" className={isDarkMode?"label dark":"label light"}>{password}</label>   
       </div>
-
-    <button className='submitBtn'>Sign In</button>
+      <div className="inputContainer">
+      <label htmlFor="checkbox" className={isDarkMode?"label dark remember":"label light remember"}>
+         <input type="checkbox" />
+         {remember}
+       </label>
+      </div>
+    <button className='submitBtn'>{signIn}</button>
     </div>
     </div>
   )

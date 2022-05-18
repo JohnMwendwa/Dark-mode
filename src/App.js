4ef989eeeccf187fe5261d29b19a40ecd4039865
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-05-18 06:29:14 +0300

    wrap all Language consumer components withe LanguageProvider element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Navbar from './Navbar';
 import PageContent from './PageContent';
+import {LanguageProvider} from './contexts/LanguageContext'
+
 function App() {
   return (
     <div className="App">
-    <PageContent>
-      <Navbar />
-    </PageContent>
+      {/* Wrap all components that are going to consume the context with the LanguageProvider */}
+      <LanguageProvider>
+        <PageContent>
+          <Navbar />
+        </PageContent>
+    </LanguageProvider>
     </div>
   );
 }

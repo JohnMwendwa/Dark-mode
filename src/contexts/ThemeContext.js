1495ffc97277fb4da64ec4fe33f238806c0a77b0
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-05-18 06:46:09 +0300

    use square brackets instead of curly brackets when setting state

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -3,7 +3,7 @@ import React,{createContext,useState} from 'react'
 export const ThemeContext = createContext();
 
 export function ThemeProvider(props) {
-    const {isDarkMode,setIsDarkMode} = useState(false);
+    const [isDarkMode,setIsDarkMode] = useState(false);
     const toggleTheme =()=>{
         setIsDarkMode(!isDarkMode);
     }

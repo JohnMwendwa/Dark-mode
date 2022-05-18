5b69722c310cc8a32adfc131e4ecadf4069a9f5a
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-05-18 10:45:12 +0300

    import theme context to toggle page background-color from light to dark

--- src/PageContent.js
+++ src/PageContent.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import {ThemeContext} from './contexts/ThemeContext';
 
 export default function PageContent(props) {
+  const {isDarkMode} = useContext(ThemeContext);
+  const styles={
+    background: isDarkMode ? "#525046" :"#fff",
+    width:"100vw",
+    height:"100vh"
+  }
   return (
-    <div>
+    <div style={styles}>
         {props.children}
     </div>
   )

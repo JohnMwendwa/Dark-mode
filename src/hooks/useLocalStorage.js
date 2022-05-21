34b5b23d9ec12d05f04a4ab33bf599962c71e81f
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-05-21 20:49:12 +0300

    create a localstorage custom hook to persist data in local storage

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,22 @@
+import {useState,useEffect} from 'react'
+
+
+function useLocalStorage(key,defaultVal) {
+const [state,setState] = useState(()=>{
+    let val;
+    try{
+    val = JSON.parse(window.localStorage.getItem(key) || String(defaultVal));
+    }catch(e){
+        val = defaultVal;
+    }
+    return val;
+});
+
+useEffect(()=>{
+    window.localStorage.setItem(key,JSON.stringify(state));
+},[key,state])
+
+  return [state,setState]
+}
+
+export default useLocalStorage
